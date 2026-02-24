445adcba5d86e3d74914fe805130f4f6950db035
ritikeshrk <thisracing@gmail.com>
2026-02-24 19:46:28 +0530

    2nd commit 😁

--- myworld/my_tennis_club/members/models.py
+++ myworld/my_tennis_club/members/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class member(models.Model): 
+    firstname=models.CharField(max_length=255)
+    lastname=models.CharField(max_length=255)
+    num=models.IntegerField(null=True)
+    joining_date=models.DateField(null=True)
 # Create your models here.
--- myworld/my_tennis_club/my_tennis_club/urls.py
+++ myworld/my_tennis_club/my_tennis_club/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('',include('members.urls')),
     path('admin/', admin.site.urls),
+    
 ]
 
